c09c4e92f77d63c6d3a8598d6650523a00572aca
mdnelles <mxnelles@gmail.com>
2022-07-20 06:38:23 -0500

    clean up

--- src/pages/Dashboard/AppBarTop.tsx
+++ src/pages/Dashboard/AppBarTop.tsx
@@ -5,8 +5,6 @@ import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
 import { ProfileNav } from "./components/ProfileNav";
 
 interface AppBarTopProps {
