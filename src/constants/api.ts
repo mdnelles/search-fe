1ec12e9b224479e9102577541183d0ce2ddf4fd1
mdnelles <mxnelles@gmail.com>
2022-07-28 13:04:54 -0500

    route fix

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,2 +1,2 @@
-export const API_REMOTE: string = "https://search.nelles.io/se";
+export const API_REMOTE: string = "https://search.nelles.io/";
 export const API_LOCAL: string = "http://localhost:5010";
